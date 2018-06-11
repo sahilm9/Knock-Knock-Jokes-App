0d887cf1746d8cceeb34640ac4862bdceb1152b6
sahilm9 <sahil.mokkapati@gmail.com>
2018-06-11 11:06:02 -0700

    included post-css and nested css

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,10 +1,12 @@
 let gulp = require('gulp'),
 postCSS = require('gulp-postcss'),
-autoPrefixer = require('autoprefixer');
+autoPrefixer = require('autoprefixer'),
+cssImport = require('postcss-import'),
+nested = require('postcss-nested');
 
 gulp.task('styles', function(){
   return gulp.src('./app/src/styles/styles.css')
-  .pipe(postCSS([autoPrefixer({ browsers: ["> 0%"] })]))
+  .pipe(postCSS([cssImport,nested,autoPrefixer({ browsers: ["> 0%"] })]))
   .on('error', function(errorInfo){
     console.log(errorInfo.toString());
     this.emit('end');
